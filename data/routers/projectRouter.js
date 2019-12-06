f244e44c9b15772ba4dc4b493a135acbc6eb7dca
Nathaniel Mosco <natemosco@gmail.com>
2019-12-06 13:26:46 -0500

    added middleware fn and GET:id to projectRouter.js

--- data/routers/projectRouter.js
+++ data/routers/projectRouter.js
@@ -6,7 +6,27 @@ const actions = require("../helpers/actionModel");
 const projectRouter = express.Router();
 
 // Middleware
-
+function validateProjectId(req, res, next) {
+  project
+    .get(req.params.id)
+    .then(project => {
+      if (!project) {
+        res
+          .status(404)
+          .json({
+            errorMessage:
+              "Project by this Id does not exist. actions cannot be added without corresponding project."
+          });
+      } else {
+        next();
+      }
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ errorMessage: "internal error, could not complete request" });
+    });
+}
 // Endpoints
 
 projectRouter.get("/", (req, res) => {
@@ -21,6 +41,21 @@ projectRouter.get("/", (req, res) => {
       res
         .status(500)
         .json({ errorMessage: "internal error fetching GET for allProjects" });
+    });
+});
+
+projectRouter.get("/:id", validateProjectId, (req, res) => {
+  projects
+    .get(req.params.id)
+    .then(singleProject => {
+      //console.log(singleProject, 'response from GET /:id');
+      res.status(200).json(singleProject);
+    })
+    .catch(error => {
+      console.log(error, "Error from get /:id");
+      res
+        .status(500)
+        .json({ errorMessage: "internal error fetching project by id" });
     });
 });
 
